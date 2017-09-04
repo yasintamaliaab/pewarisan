e839d0f190e207e1e9e418e65126c16899cce710
User <User@192.168.56.2>
2017-09-04 10:12:58 +0700

    Materi pewarisan

---
+++ src/Praktikum/TestIndukTurunan.java
@@ -0,0 +1,8 @@
+package Praktikum;
+public class TestIndukTurunan {
+    public static void main(String[]args){
+        Turunan obj= new Turunan();
+        obj.test("Method Overload");
+        obj.test();
+    }
+}
