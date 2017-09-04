add3b1062192208e0e97e477cf856b68c9186eb1
User <User@192.168.56.2>
2017-09-04 10:58:22 +0700

    Materi pewarisan

--- src/Latihan6/praktikum1.java
+++ src/Latihan6/praktikum1.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Latihan6;
-
-/**
- *
- * @author User
- */
 public class praktikum1 {
     static int tambah(int a, int b){
     return a+b;
@@ -26,8 +16,6 @@ public class praktikum1 {
     double d =0.5;
     
     System.out.println("1. "+(a+b));
-    System.out.println("2. a<"+a+"> +"+b+"> = "+(a+b));
+    System.out.println("2. a<"+a+"> +"+" b<"+b+"> = "+(a+b));
     System.out.println("3. "+(a+b+c));
-    System.out.println("4. "+(a+b+c+d));
-    }
-}
+    System.out.println("4. "+(a+b+c+d));}}
